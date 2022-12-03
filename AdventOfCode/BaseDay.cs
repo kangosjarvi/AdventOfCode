7493d6f3ef794e99c284ade010044bc96e31e5b0
Jim Kangosjarvi <jim.kangosjarvi@skf.com>
2022-12-04 00:48:26 +0200

    first commit

---
+++ AdventOfCode/BaseDay.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode.Interface;
+
+namespace AdventOfCode
+{
+    internal abstract class BaseDay : IDay
+    {
+        public virtual void Play()
+        {
+            Console.WriteLine(this);
+        }
+        public abstract override string ToString();
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Code._2022;
+using AdventOfCode.Interface;
+
+namespace AdventOfCode;
+internal static class Program
+{
+    private static void Main()
+    {
+        IDay day = new Day1();
+        day.Play();
+
+        day = new Day2();
+        day.Play();
+
+        day = new Day3();
+        day.Play();
+
+    }
+}
