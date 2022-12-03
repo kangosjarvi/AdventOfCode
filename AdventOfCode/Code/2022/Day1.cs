010eb510ef85f99e0ab3cedef2a0fecb20461bd8
Jim Kangosjarvi <jim.kangosjarvi@skf.com>
2022-12-04 00:55:43 +0200

    Shortenup

--- AdventOfCode/Code/2022/Day1.cs
+++ AdventOfCode/Code/2022/Day1.cs
@@ -1,26 +1,24 @@
-﻿namespace AdventOfCode.Code._2022
+﻿namespace AdventOfCode.Code._2022;
+internal class Day1 : BaseDay
 {
-    internal class Day1 : BaseDay
+    int _maxCalories;
+    int _totalCaloriesForTopThree;
+    public override void Play()
     {
-        int _maxCalories;
-        int _totalCaloriesForTopThree;
-        public override void Play()
+        //Will show 25th december
+        int elf = 1;
+        int value = 0;
+        Dictionary<int, int> elves = new Dictionary<int, int>();
+        foreach (string line in File.ReadLines("job/2022-1.txt"))
         {
-            //Will show 25th december
-            int elf = 1;
-            int value = 0;
-            Dictionary<int, int> elves = new Dictionary<int, int>();
-            foreach (string line in File.ReadLines("job/2022-1.txt"))
-            {
 
-            }
-
-            base.Play();
         }
 
-        public override string ToString()
-        {
-            return $"{this.GetType().Name}, max calories a elf carries is {_maxCalories} and top 3 carry {_totalCaloriesForTopThree}";
-        }
+        base.Play();
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name}, max calories a elf carries is {_maxCalories} and top 3 carry {_totalCaloriesForTopThree}";
     }
 }
--- AdventOfCode/Code/2022/Day2.cs
+++ AdventOfCode/Code/2022/Day2.cs
@@ -1,32 +1,31 @@
-﻿namespace AdventOfCode.Code._2022
+﻿namespace AdventOfCode.Code._2022;
+internal class Day2 : BaseDay
 {
-    internal class Day2 : BaseDay
+    private const int WinnerPoints = 6;
+    private const int DrawPoints = 3;
+    private int _secretTotalScore;
+    private int _totalScore;
+
+
+
+    public override void Play()
     {
-        private const int WinnerPoints = 6;
-        private const int DrawPoints = 3;
-        private int _secretTotalScore;
-        private int _totalScore;
-
-
-
-        public override void Play()
+        //Will show 25th december
+        foreach (string line in File.ReadLines("job/2022-2.txt"))
         {
-            //Will show 25th december
-            foreach (string line in File.ReadLines("job/2022-2.txt"))
-            {
-                var game = line.Split(" ");
+            var game = line.Split(" ");
 
-          //      _totalScore += StrategyGuide(game[0], game[1], false);
-          //      _secretTotalScore += StrategyGuide(game[0], game[1], true);
+        //      _totalScore += StrategyGuide(game[0], game[1], false);
+        //      _secretTotalScore += StrategyGuide(game[0], game[1], true);
 
-            }
-            base.Play();
         }
-
-        public override string ToString()
-        {
-            return $"{GetType().Name}, your total normal score is {_totalScore} and secret strategy score {_secretTotalScore}";
-        }
+        base.Play();
     }
 
+    public override string ToString()
+    {
+        return $"{GetType().Name}, your total normal score is {_totalScore} and secret strategy score {_secretTotalScore}";
+    }
 }
+
+
